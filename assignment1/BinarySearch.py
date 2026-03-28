912295df7366874bfb7a72fae95f78a1e4e5bdd4
Nitinkr07 <nitin.kr3107@gmail.com>
2026-03-28 15:06:04 +0530

    Add files via upload

---
+++ assignment1/BinarySearch.py
@@ -0,0 +1,16 @@
+def binary_search(arr, low, high, key):
+    if low > high:
+        return -1
+
+    mid = (low + high) // 2
+
+    if arr[mid] == key:
+        return mid
+    elif key < arr[mid]:
+        return binary_search(arr, low, mid - 1, key)
+    else:
+        return binary_search(arr, mid + 1, high, key) 
+    
+
+arr = [1, 3, 5, 7, 9]
+print(binary_search(arr, 0, len(arr)-1, 7))   
